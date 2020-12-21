af916d6c3c1e6d986d915a292ea191cfa37d7dd2
boss0703 <prinnykurtis002@gmail.com>
2020-12-21 11:34:55 +0900

    initial

---
+++ ABC081B.py
@@ -0,0 +1,9 @@
+num = input()
+nums = list(map(int, input().split()))
+cnt = 0
+
+while 1 not in [i % 2 for i in nums]:
+    cnt += 1
+    nums = [d / 2 for d in nums]
+
+print(cnt)
---
+++ main.py
@@ -0,0 +1,6 @@
+num = input()
+cnt = 0
+for i in list(num):
+    if i == '1':
+        cnt += 1
+print(cnt)
