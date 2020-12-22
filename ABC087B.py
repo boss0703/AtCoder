2e378810fe8ead4f84f642a26347e2e7c268e5e3
boss0703 <prinnykurtis002@gmail.com>
2020-12-22 13:33:30 +0900

    coin問題完了

--- ABC087B.py
+++ ABC087B.py
@@ -1,8 +1,11 @@
-a = input()
-b = input()
-c = input()
-x = input()
-
-# 足りない場合終了
-if x > a * 500 + b * 100 + c + 50:
-    print(0)
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
+ans = 0
+for i in range(a + 1):
+    for j in range(b + 1):
+        for k in range(c + 1):
+            if 500 * i + 100 * j + 50 * k == x:
+                ans += 1
+print(ans)
