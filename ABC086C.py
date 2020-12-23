278d5ab984dea928798e2d2013e03dc1a935b0e4
boss0703 <prinnykurtis002@gmail.com>
2020-12-24 04:08:46 +0900

    traveling問題完了

---
+++ ABC086C.py
@@ -0,0 +1,22 @@
+# t x y
+# 移動可能距離を超過してないかチェック t >= |x| + |y| 絶対値abs()
+# 全部合計して偶数ならYES
+n = int(input())
+l = [list(map(int, input().split())) for i in range(n)]
+ans = ''
+for i in range(0, n):
+    ans = 'No'
+    if i == 0:
+        if l[i][0] >= (l[i][1] + l[i][2]):
+            if sum(l[i]) % 2 == 0:
+                ans = 'Yes'
+    else:
+        if l[i][0] - l[i-1][0] >= abs(l[i][1] - l[i-1][1]) + abs(l[i][2] - l[i - 1][2]):
+            if (l[i][0] - l[i-1][0] + l[i][1] - l[i-1][1] + abs(l[i][2] - l[i - 1][2])) % 2 == 0:
+                ans = 'Yes'
+    if ans == 'No':
+        break
+print(ans)
+# 奇数偶数判定
+
+#
